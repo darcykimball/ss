40c824c4836c2b1a6a83600e6a11201ce5018d54
Darcy Kimball <kevinzbao@gmail.com>
2018-02-09 18:08:49 +0000

    Added server (driver) stub

---
+++ server.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+
+int main(int argc, char** argv) { 
+  using namespace std::literals;
+
+
+  auto print_usage = [prog_name = argv[0]]() {
+    std::cerr << "Usage: " << prog_name <<
+      " -document_root directory -port port_no\n";
+  };
+
+
+  // Parse/validate args
+  if (argc != 5 || argv[1] != "-document_root"s || argv[3] != "-port"s) {
+    print_usage();
+    std::exit(EXIT_FAILURE);
+  }
+
+
+  uint16_t port_no;
+  if (!(std::istringstream{argv[4]} >> port_no)) {
+    std::cerr << "Error parsing port_no!\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+
+  std::cout << "Starting server on port " << port_no << "...\n";
+
+
+  return EXIT_SUCCESS;
+}
