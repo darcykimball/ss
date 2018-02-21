b6beb9b3ba2cf610a3d0cefc6ab3d71894bc9b05
Darcy Kimball <kevinzbao@gmail.com>
2018-02-21 07:48:49 +0000

    Added file-fetcher component

---
+++ fetcher.hpp
@@ -0,0 +1,92 @@
+#ifndef FETCHER_HPP
+#define FETCHER_HPP
+
+
+#include <exception>
+#include <iostream>
+#include <memory>
+
+#include <boost/filesystem.hpp>
+
+#include "http.hpp"
+
+
+namespace http {
+
+
+using namespace std::literals;
+using namespace boost::filesystem;
+
+
+// Fetcher exception type
+struct fetch_error : std::runtime_error {
+  status_code code;
+
+  fetch_error(std::string const& msg) : std::runtime_error{msg} {}
+  fetch_error(status_code c) : std::runtime_error{"HTTP error"s}, code{c} {}
+};
+
+
+// A HTTP resource (file) fetcher
+class fetcher {
+
+  /// FIXME: Abs or rel??
+  path const _root; // Path to root directory
+
+
+public:
+  
+  fetcher(std::string const& root_path) : _root(root_path) {
+    try {
+      // TODO check that it's a directory
+      if (!exists(_root) || !is_directory(_root)) {
+        // TODO: throw user exception
+        throw fetch_error{"Root directory not found!"s};
+      }
+
+    } catch (filesystem_error const& e) {
+      // Something went horribly wrong on our end
+      std::cerr << "fetcher(): " << e.what() << '\n';
+      throw fetch_error{status_code::internal_server_error};
+    }
+  }
+
+
+  // Fetch a resource based on relative path
+  // Throws an exception if couldn't fetch resource for whatever reason
+  std::unique_ptr<ifstream> fetch(path rel_path) {
+    // Make path wrt root  
+    path abs_path{_root};
+    abs_path /= rel_path;
+
+
+    try {
+      // Check existence of resource
+      if (!exists(rel_path) || !is_regular_file(rel_path)) {
+        throw fetch_error{status_code::not_found};
+      } 
+      
+      // Check permissions
+      // TODO/FIXME
+      if (true) {
+        throw fetch_error{status_code::forbidden};
+      }
+    } catch (filesystem_error const& e) {
+      // Something went horribly wrong on our end
+      std::cerr << "fetcher::fetch(): " << e.what() << '\n';
+      throw fetch_error{status_code::internal_server_error};
+    }
+
+
+    return std::make_unique<ifstream>(abs_path);
+  }
+
+
+
+};
+
+
+} // namespace http
+
+
+#endif // FETCHER_HPP
